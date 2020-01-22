df483dad3cdf36fd72567dc54b05ced48135ef8a
Castile <zhl396740445@163.com>
2020-01-22 18:04:48 +0800

    增加了可扩展性的方法

--- leetcode/src/Arrays/leetcode_80_remove_duplicates_from_sorted_array_ii.java
+++ leetcode/src/Arrays/leetcode_80_remove_duplicates_from_sorted_array_ii.java
@@ -1,5 +1,4 @@
 package Arrays;
-
 /**
  * @author Hongliang Zhu
  * @create 2020-01-22 10:07
@@ -28,10 +27,26 @@ public class leetcode_80_remove_duplicates_from_sorted_array_ii {
         return p;
     }
 
+    public static int removeDuplicates2(int[] nums) {
+        if (nums.length <= 2){
+            return nums.length;
+        }
+        int index = 3;
+        for(int i = 3; i < nums.length; i++){
+            if(nums[index-3] != nums[i]){
+                nums[index++] = nums[i];
+            }
+        }
+
+
+        return index;
+    }
+
+
     public static void main(String[] args) {
 
-        int []nums = {0,0,1,1,1,1,2,3,3};
-        int len = removeDuplicates(nums);
+        int []nums = {0,0,0,1,1,1,1,1,1,1,2,3,3};
+        int len = removeDuplicates2(nums);
         System.out.println(len);
         for (int i = 0; i < len; i++) {
             System.out.print(nums[i]+",");
