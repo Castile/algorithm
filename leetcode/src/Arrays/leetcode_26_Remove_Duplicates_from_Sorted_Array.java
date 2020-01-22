6c65c088a993ed5f1d143251baef29897814fdd3
Castile <zhl396740445@163.com>
2020-01-22 17:28:49 +0800

    leetcode-26

---
+++ leetcode/src/Arrays/leetcode_26_Remove_Duplicates_from_Sorted_Array.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+/**
+ * @author Hongliang Zhu
+ * @create 2020-01-22 17:17
+ */
+public class leetcode_26_Remove_Duplicates_from_Sorted_Array {
+
+    public int removeDuplicates(int[] nums) {
+        if(nums.length == 0){
+            return 0;
+        }
+        int p = 0;
+        int q = 1;
+        while(q < nums.length){
+            if(nums[p] == nums[q]){
+                q++;
+            }else{
+
+                nums[p+1] = nums[q];
+                p++;
+            }
+        }
+
+        return p + 1 ;
+
+    }
+
+
+}
