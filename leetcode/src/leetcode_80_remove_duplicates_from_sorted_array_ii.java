c40d05aec9471ba304d9a2109a0c453d9572d4d9
Castile <zhl396740445@163.com>
2020-01-22 17:13:32 +0800

    leetcode-80:删除排序数组中的重复项 II

---
+++ leetcode/src/leetcode_80_remove_duplicates_from_sorted_array_ii.java
@@ -0,0 +1,42 @@
+/**
+ * @author Hongliang Zhu
+ * @create 2020-01-22 10:07
+ */
+public class leetcode_80_remove_duplicates_from_sorted_array_ii {
+
+    public static int removeDuplicates(int[] nums) {
+        if(nums.length == 0)
+            return 0;
+        int p = 1;
+        int c = 1; // 计数
+        for(int i = 1; i < nums.length;  i++){
+            if(nums[i] == nums[i-1]){
+                c++;
+            }else {
+                c = 1;  //复位
+            }
+
+            if( c <= 2) {
+                nums[p] = nums[i];
+                p++;
+            }
+
+        }
+
+        return p;
+    }
+
+    public static void main(String[] args) {
+
+        int []nums = {0,0,1,1,1,1,2,3,3};
+        int len = removeDuplicates(nums);
+        System.out.println(len);
+        for (int i = 0; i < len; i++) {
+            System.out.print(nums[i]+",");
+        }
+        System.out.println();
+
+    }
+
+
+}
